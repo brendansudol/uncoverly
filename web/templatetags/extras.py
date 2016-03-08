e5c694ccb975cccd67a2a31a3bdaee5e9b1ef264
brendan sudol <brendansudol@gmail.com>
2016-03-08 00:40:43 -0500

    iterate on product card

---
+++ web/templatetags/extras.py
@@ -0,0 +1,14 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def make_url(url, page, query=None):
+    path = '{}?{}{}'.format(
+        url,
+        'q={}&'.format(query) if query else '',
+        'page={}'.format(page)
+    )
+    return path
