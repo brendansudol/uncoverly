3aed03c327d19c05c8b93778ea973d444053ca0b
brendan sudol <brendansudol@gmail.com>
2016-03-14 01:14:11 -0400

    add price filter

--- web/views/home.py
+++ web/views/home.py
@@ -12,10 +12,18 @@ class HomeView(ListView):
 
     def get_queryset(self):
         qs = super(HomeView, self).get_queryset()
+        get = self.request.GET
 
-        q = self.request.GET.get('q')
+        q = get.get('q')
         if q is not None:
             qs = self.model.objects.search(q)
+
+        p = get.get('price')
+        if p is not None:
+            try:
+                qs = qs.filter(price_usd__lte=(int(p) * 100))
+            except Exception:
+                pass
 
         return qs
 
