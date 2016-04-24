c3e9d7e6bfaa70b64674d94013ccedbfeb16fc42
brendan sudol <brendansudol@gmail.com>
2016-04-24 17:24:50 -0400

    better navbar, seller page start

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -9,6 +9,10 @@ require('../sass/app.scss');
  * js
  */
 
+require('./vendor/collapse');
+require('./vendor/dropdown');
 require('./vendor/modal');
+
+
 require('./modules/favorite');
 require('./modules/newsletter');
