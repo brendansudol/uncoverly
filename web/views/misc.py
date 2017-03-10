f5d64513d04af42ee65b97432c1e628f3cd74d4c
brendan sudol <brendansudol@gmail.com>
2017-03-10 00:54:10 -0500

    add intro banner

--- web/views/misc.py
+++ web/views/misc.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 
+from web.models import Product
+
 
 class ContactView(TemplateView):
     template_name = 'web/contact.html'
@@ -7,6 +9,11 @@ class ContactView(TemplateView):
 
 class LabsView(TemplateView):
     template_name = 'web/labs.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(LabsView, self).get_context_data(**kwargs)
+        context['products'] = Product.objects.filter(is_visible=True).all()[:9]
+        return context
 
 
 class PrivacyView(TemplateView):
